8cb080f35b1fcfae58451a6e894828b16a880dde
carstingaxion <carstingaxion@users.noreply.github.com>
2023-10-09 23:16:49 +0000

    ✅ Ready to clone and code.

--- inc/namespace.php
+++ inc/namespace.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Figuren_Theater project_name.
+ * Figuren_Theater theater.
  *
- * @package figuren-theater/project_urlname
+ * @package figuren-theater/theater
  */
 
-namespace Figuren_Theater\project_name;
+namespace Figuren_Theater\theater;
 
 use Altis;
 
@@ -24,9 +24,9 @@ function register() :void {
 	];
 
 	Altis\register_module(
-		'project_urlname',
+		'theater',
 		DIRECTORY,
-		'project_name',
+		'theater',
 		$options,
 		__NAMESPACE__ . '\\bootstrap'
 	);
